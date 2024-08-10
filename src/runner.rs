ba606c15339806c3e438decbf7ef6f2bcfa22a30
Jason Lee <lwlee2608@gmail.com>
2024-08-10 15:49:43 +0800

    Initial commit for multi scenario support

--- src/runner.rs
+++ src/runner.rs
@@ -4,13 +4,20 @@ use crate::options::Options;
 use crate::scenario;
 use diameter::transport::DiameterClient;
 use diameter::transport::DiameterClientConfig;
+use diameter::DiameterMessage;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::Arc;
 use std::time::Instant;
+use tokio::sync::mpsc::channel;
+use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::task;
 use tokio::task::LocalSet;
 use tokio::time::{self, sleep, Duration};
+
+// pub struct Runner {
+//     param: RunParameter,
+// }
 
 #[derive(Clone)]
 pub struct RunParameter {
@@ -58,12 +65,19 @@ pub async fn run(options: Options, param: RunParameter) -> RunReport {
     )
     .unwrap();
 
-    let mut repeating_scenario = scenario::Scenario::new(
-        options.scenarios.get(1).unwrap(),
-        &global,
-        Arc::clone(&dict),
-    )
-    .unwrap();
+    // Skip first scenario, which is hardcoded as Init scenario for now
+    let mut repeating_scenarios = vec![];
+    for scenario in options.scenarios.iter().skip(1) {
+        let s = scenario::Scenario::new(scenario, &global, Arc::clone(&dict)).unwrap();
+        repeating_scenarios.push(s);
+    }
+
+    // let mut ccri_scenario = scenario::Scenario::new(
+    //     options.scenarios.get(1).unwrap(),
+    //     &global,
+    //     Arc::clone(&dict),
+    // )
+    // .unwrap();
 
     // TODO
     // let mut ccrt_scenario =
@@ -84,17 +98,22 @@ pub async fn run(options: Options, param: RunParameter) -> RunReport {
                 DiameterClient::handle(&mut handler, dict_ref).await;
             });
 
-            // Init, Init scenario
+            // Init scenario, send CER
             let cer = init_scenario.next_message().unwrap();
             if options.log_requests {
                 log::info!("CER: {}", cer);
             }
             let resp = client.send_message(cer).await.unwrap();
             let cea = resp.await.unwrap();
-
             if options.log_responses {
                 log::info!("CEA: {}", cea);
             }
+
+            // Event Loop
+            let (eventloop_tx, eventloop_rx) = channel(32);
+            tokio::spawn(async move {
+                event_loop(client, eventloop_rx).await.unwrap();
+            });
 
             // Start Repeating Scenario
             log::info!(
@@ -104,39 +123,76 @@ pub async fn run(options: Options, param: RunParameter) -> RunReport {
                 param.batch_size,
                 param.interval.as_secs_f64()
             );
-            let mut interval = time::interval(param.interval);
+
+            // let mut interval = time::interval(param.interval);
 
             let start = Instant::now();
 
-            // We don't need atomic operation since we are running inside LocalSet
-            let counter: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
+            let (resp_tx, mut resp_rx) = channel(32);
 
-            for _ in 0..param.total_iterations / param.batch_size {
-                interval.tick().await;
+            let ccri_scenario = repeating_scenarios.get_mut(0).unwrap();
+            let ccri = ccri_scenario.next_message().unwrap();
+            eventloop_tx
+                .send(Event::SendMessage(ccri, resp_tx.clone()))
+                .await
+                .unwrap();
 
-                for _ in 0..param.batch_size {
-                    // let ccr = ccr(client.get_next_seq_num());
-                    let ccr = repeating_scenario.next_message().unwrap();
-                    if options.log_requests {
-                        log::info!("Request: {}", ccr);
-                    }
-
-                    let counter = Rc::clone(&counter);
-                    let resp = client.send_message(ccr).await.unwrap();
-                    let _ = task::spawn_local(async move {
-                        let cca = resp.await.expect("Failed to get response");
-                        if options.log_responses {
-                            log::info!("Response: {}", cca);
-                        }
-                        *counter.borrow_mut() += 1;
-                    });
-                }
+            if let Some(ccai) = resp_rx.recv().await {
+                log::info!("Received CCA-I: {}", ccai);
             }
 
-            log::info!("Waiting for all requests to finish");
-            while *counter.borrow() < param.total_iterations {
-                sleep(Duration::from_millis(50)).await;
+            let ccrt_scenario = repeating_scenarios.get_mut(1).unwrap();
+            let ccrt = ccrt_scenario.next_message().unwrap();
+            eventloop_tx
+                .send(Event::SendMessage(ccrt, resp_tx.clone()))
+                .await
+                .unwrap();
+
+            if let Some(ccat) = resp_rx.recv().await {
+                log::info!("Received CCA-T: {}", ccat);
             }
+
+            // for _ in 0..total_iterations {
+            //     interval.tick().await;
+            // }
+
+            //
+            // // We don't need atomic operation since we are running inside LocalSet
+            // let counter: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
+            //
+            // for _ in 0..param.total_iterations / param.batch_size {
+            //     interval.tick().await;
+            //
+            //     for _ in 0..param.batch_size {
+            //         // let ccr = ccr(client.get_next_seq_num());
+            //         let ccr = repeating_scenario.next_message().unwrap();
+            //         if options.log_requests {
+            //             log::info!("Request: {}", ccr);
+            //         }
+            //
+            //         let counter = Rc::clone(&counter);
+            //         let resp = client.send_message(ccr).await.unwrap();
+            //         let _ = task::spawn_local(async move {
+            //             let cca = resp.await.expect("Failed to get response");
+            //             if options.log_responses {
+            //                 log::info!("Response: {}", cca);
+            //             }
+            //             *counter.borrow_mut() += 1;
+            //         });
+            //     }
+            // }
+            //
+            // log::info!("Waiting for all requests to finish");
+            // while *counter.borrow() < param.total_iterations {
+            //     sleep(Duration::from_millis(50)).await;
+            // }
+            //
+
+            // sleep 1
+            // sleep(Duration::from_secs(1)).await;
+
+            // Terminate the event loop
+            eventloop_tx.send(Event::Terminate).await.unwrap();
 
             let elapsed = start.elapsed();
             let elapsed_s = elapsed.as_secs() as f64 + elapsed.subsec_millis() as f64 / 1000.0;
@@ -146,6 +202,41 @@ pub async fn run(options: Options, param: RunParameter) -> RunReport {
             RunReport { tps, elapsed }
         })
         .await
+}
+
+// struct EventContext {
+//     scenario_id: usize,
+// }
+
+enum Event {
+    SendMessage(DiameterMessage, Sender<DiameterMessage>),
+    Terminate,
+}
+
+async fn event_loop(
+    mut client: DiameterClient,
+    mut rx: Receiver<Event>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    while let Some(event) = rx.recv().await {
+        match event {
+            Event::SendMessage(request, tx) => {
+                log::info!("Sending message: {}", request);
+                // send message
+                let resp = client.send_message(request).await.unwrap();
+                let response = resp.await.unwrap();
+
+                // log::info!("Received response: {}", response);
+
+                // Send response back to main runner loop
+                tx.send(response).await.unwrap();
+            }
+            Event::Terminate => {
+                log::info!("Terminating event loop");
+                break;
+            }
+        }
+    }
+    todo!()
 }
 
 #[cfg(test)]
